76a009cc63f6a575193db1e80e3035acd03321c7
kolaatabaew <kolaatabaev8688@gmail.com>
2026-05-01 23:04:37 +0300

    resl class

--- src/Pizza.php
+++ src/Pizza.php
@@ -1,36 +1,23 @@
 <?php
+
 namespace PizzaLib;
 
-class Pizza {
-    protected string $name = "Стандартная пицца";
-    protected string $sauce = "Томатный";
-    protected array $toppings = [];
+abstract class Pizza
+{
+    public string $name;
+    public string $sauce;
+    /** @var string[] */
+    public array $toppings;
 
-    public function prepare(): void {
-        echo "--- Подготовка {$this->name} ---\n";
-        echo "Добавление соуса: {$this->sauce}...\n";
-        echo "Добавление добавок: " . implode(", ", $this->toppings) . "\n";
+    public function prepare(): void
+    {
+        echo "Началась готовка пиццы {$this->name}\n";
+        echo "Добавлен соус {$this->sauce}\n";
+        echo "Добавлены топики: " . implode(", ", $this->toppings) . "\n";
     }
 
-    public function bake(): void { echo "Выпекание: 25 мин при 350\n"; }
-    public function cut(): void { echo "Нарезка: по диагонали\n"; }
-    public function box(): void { echo "Упаковка: в фирменную коробку\n"; }
-    public function getName(): string { return $this->name; }
-}
-
-// Реальные классы
-class MoscowCheesePizza extends Pizza {
-    public function __construct() {
-        $this->name = "Сырная Москва";
-        $this->sauce = "Сливочный";
-        $this->toppings[] = "Моцарелла";
-    }
-}
-
-class MoscowPepperoniPizza extends Pizza {
-    public function __construct() {
-        $this->name = "Пепперони Столичная";
-        $this->sauce = "Томатный острый";
-        $this->toppings[] = "Колбаски";
+    public function cut(): void
+    {
+        echo "Данную пиццу нарезают по диагонали\n";
     }
 }
--- src/PizzaStore.php
+++ src/PizzaStore.php
@@ -1,18 +1,17 @@
 <?php
+
 namespace PizzaLib;
 
-class PizzaStore {
-    // Убрали abstract, добавили базовую логику
-    protected function createPizza(string $type): Pizza {
-        return new Pizza();
-    }
+abstract class PizzaStore
+{
+    abstract protected function createPizza(string $type): Pizza;
 
-    public function orderPizza(string $type): Pizza {
+    public function orderPizza(string $type): Pizza
+    {
         $pizza = $this->createPizza($type);
         $pizza->prepare();
-        $pizza->bake();
         $pizza->cut();
-        $pizza->box();
+
         return $pizza;
     }
 }
